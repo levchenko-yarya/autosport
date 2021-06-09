e902bf74821eee4cdf775dfde7da8235040f45cd
levchenko-yarya <levchenkoyarya@gmail.com>
2021-06-09 22:07:22 +0300

    fix state img

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,4 @@
+//  import '../../public/assets/team/mersedes.jpg'
 let state = {
     duelPage: {
         duels: [
@@ -113,16 +114,16 @@ let state = {
     },
     teamPage: {
         teams: [
-            {id: 1, name: 'Мерседес', img: '../../../../assets/team/mersedes.jpg'},
-            {id: 2, name: 'Ред Булл', img: '../../../../assets/team/redbull.jpg'},
-            {id: 3, name: 'Макларен', img: '../../../../assets/team/mclaren.jpg'},
-            {id: 4, name: 'Феррари', img: '../../../../assets/team/ferrari.jpg'},
-            {id: 5, name: 'Альпин', img: '../../../../assets/team/alpine.jpg'},
-            {id: 6, name: 'Альфа Таури', img: '../../../../assets/team/tauri.jpg'},
-            {id: 7, name: 'Астон Мартин', img: '../../../../assets/team/aston.jpg'},
-            {id: 8, name: 'Альфа Ромео', img: '../../../../assets/team/romeo.jpg'},
-            {id: 9, name: 'Вильямс', img: '../../../../assets/team/williams.jpg'},
-            {id: 10, name: 'Хаас', img: '../../../../assets/team/haas.jpg'}
+            {id: 1, name: 'Мерседес', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/mercedes.png.transform/4col/image.png'},
+            {id: 2, name: 'Ред Булл', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/red-bull-racing.png.transform/4col/image.png'},
+            {id: 3, name: 'Макларен', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/mclaren.png.transform/4col/image.png'},
+            {id: 4, name: 'Феррари', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/ferrari.png.transform/4col/image.png'},
+            {id: 5, name: 'Альпин', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/alpine.png.transform/4col/image.png'},
+            {id: 6, name: 'Альфа Таури', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/alphatauri.png.transform/4col/image.png'},
+            {id: 7, name: 'Астон Мартин', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/aston-martin.png.transform/4col/image.png'},
+            {id: 8, name: 'Альфа Ромео', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/alfa-romeo-racing.png.transform/4col/image.png'},
+            {id: 9, name: 'Вильямс', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/williams.png.transform/4col/image.png'},
+            {id: 10, name: 'Хаас', img: 'https://www.formula1.com/content/dam/fom-website/teams/2021/haas-f1-team.png.transform/4col/image.png'}
         ]
     }
 }
