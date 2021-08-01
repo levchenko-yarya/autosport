e5afff107195f9e3bda05e097d71d3df8038348e
levchenko-yarya <levchenkoyarya@gmail.com>
2021-08-01 14:15:54 +0300

    view graphql driver

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema')
 
 const app = express()
+app.use(cors())
 
 let uri = 'mongodb://blog-db:b75EPfKQfG5dPPnw5JjbDa9ggH5cwJdiAcj0FiR4Z2hvgv1j0Hbd9PFlGiGASEKxMN48yP3UXR9xDCyMW6L4Jg==@blog-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@blog-db@'
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,82 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
 
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    // useMutation,
+    gql
+} from "@apollo/client"
+
+const client = new ApolloClient({
+    uri: "http://localhost:5000/graphql",
+    cache: new InMemoryCache()
+})
+
+const DRIVER = gql`
+    query {
+        driver {
+            id,
+            fullname 
+        }
+    }`
+
+// const ADD_DRIVER = gql`
+//     mutation AddDriver($fullname: String!) {
+//             addDriver(fullname: $fullname)
+//         }
+//     }`
+
+function DriverList() {
+    const {loading, error, data} = useQuery(DRIVER);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.driver.map(({id, fullname}) => (
+        <div>
+            <p>
+                {id}: {fullname}
+            </p>
+        </div>
+    ));
+}
+
+// function AddDriver() {
+//     let input
+//     const [addDriver, { data, loading, error }] = useMutation(ADD_DRIVER)
+//
+//     if (loading) return 'Submitting...'
+//     if (error) return `Submission error! ${error.message}`
+//
+//     return (
+//         <div>
+//             <form
+//                 onSubmit={e => {
+//                     e.preventDefault();
+//                     addDriver({ variables: { text: input.value } });
+//                     input.value = '';
+//                 }}
+//             >
+//                 <input
+//                     ref={node => {
+//                         input = node;
+//                     }}
+//                 />
+//                 <button type="submit">Add Driver</button>
+//             </form>
+//         </div>
+//     )
+// }
+
 ReactDOM.render(
-  <App state={state}/>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        {/*<App state={state}/>*/}
+        <DriverList/>
+    </ApolloProvider>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
