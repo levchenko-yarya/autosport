ed6aad738f51c08757af8a7b79ad03e40ce1ca07
levchenko-yarya <levchenkoyarya@gmail.com>
2021-10-18 20:27:18 +0300

    get delete driver

--- server/driver/driver.controller.js
+++ server/driver/driver.controller.js
@@ -2,23 +2,28 @@ const Driver = require('../models/driver')
 
 exports.all = (req, res) => {
     Driver.find({}, (err, drivers) => {
-        if (err) {
-            return res.status(400)
-        }
-        res.render('driver/get.hbs', {
-            drivers: drivers
-        })
+        if (err) return res.status(400)
+        res.render('driver/all.hbs', {drivers: drivers})
     })
 }
 
-exports.add = (req, res) => {
-    res.render('driver/add.hbs')
+exports.get = (req, res) => {
+    Driver.findById(req.params.id, (err, driver) => {
+        if (!driver) return res.send({error: 'Not Found'})
+        if (!err) return res.render('driver/get.hbs', {driver: driver})
+        else return res.send({error: 'Server error'})
+    })
 }
+
+exports.add = (req, res) => {res.render('driver/add.hbs')}
 
 exports.store = (req, res) => {
-    const driver = new Driver({
-        fullname: req.body.fullname
-    })
+    const driver = new Driver({fullname: req.body.fullname})
     driver.save()
+    res.redirect('/')
+}
+
+exports.destroy = (req, res) => {
+    Driver.findOneAndDelete(req.params.id)
     res.redirect('/')
 }
--- server/driver/driver.router.js
+++ server/driver/driver.router.js
@@ -3,7 +3,9 @@ const controller = require('./driver.controller')
 const router = express.Router()
 
 router.get('/all', controller.all)
+router.get('/:id', controller.get)
 router.get('/add', controller.add)
 router.post('/add', controller.store)
+router.use('/delete/:id', controller.destroy)
 
 module.exports = router
