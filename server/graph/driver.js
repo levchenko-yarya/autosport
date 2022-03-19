746b70e2a4d23365734aba51a1c26dedde0b8278
levchenkoyarya <levchenkoyarya@gmail.com>
2022-03-19 22:54:14 +0200

    graph folder

--- server/graph/driver.js
+++ server/graph/driver.js
@@ -1,21 +1,13 @@
 const graphql = require('graphql')
 const {GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList, GraphQLSchema, GraphQLNonNull} = graphql
 
-const Driver = require('./models/driver')
-const Team = require('./models/team')
+const Driver = require('../models/driver')
 
 const DriverType = new GraphQLObjectType({
     name: 'Driver',
     fields: () => ({
         id: {type: GraphQLID},
         fullname: {type: GraphQLString}
-    })
-})
-const TeamType = new GraphQLObjectType({
-    name: 'Team',
-    fields: () => ({
-        id: {type: GraphQLID},
-        teamName: {type: GraphQLString}
     })
 })
 
@@ -31,19 +23,10 @@ const RootQuery = new GraphQLObjectType({
                 }
                 return Driver.find({'_id': args.id})
             }
-        },
-        team: {
-            type: new GraphQLList(TeamType),
-            args: {id: {type: GraphQLID}},
-            resolve(parent, args) {
-                if (!args.id) {
-                    return Team.find({})
-                }
-                return Team.find({'_id': args.id})
-            }
         }
     }
 })
+
 
 const Mutation = new GraphQLObjectType({
     name: 'Mutations',
@@ -79,40 +62,6 @@ const Mutation = new GraphQLObjectType({
             },
             resolve(parent, args) {
                 return Driver.deleteOne({_id: args.id})
-            }
-        },
-
-        addTeam: {
-            type: TeamType,
-            args: {
-                teamName: {type: GraphQLString}
-            },
-            resolve(parent, args) {
-                let team = new Team({
-                    teamName: args.teamName
-                })
-                return team.save()
-            }
-        },
-        updateTeam: {
-            type: TeamType,
-            args: {
-                id: {type: GraphQLID},
-                teamName: {type: GraphQLString}
-            },
-            resolve(parent, args) {
-                return Team.findByIdAndUpdate(args.id, {
-                    teamName: args.teamName
-                })
-            }
-        },
-        deleteTeam: {
-            type: TeamType,
-            args: {
-                id: {type: GraphQLID}
-            },
-            resolve(parent, args) {
-                return Team.deleteOne({_id: args.id})
             }
         }
     }
